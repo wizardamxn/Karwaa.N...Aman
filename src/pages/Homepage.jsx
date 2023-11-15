a1bda56f63fbdb26884396312adb59545712f8c4
wizardamxn <148276445+wizardamxn@users.noreply.github.com>
2023-11-15 15:18:15 +0530

    gggss

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -21,7 +21,7 @@ function Homepage() {
       {/* home page  */}
       <div id="home" className="px-12 pt-20 gap-44 items-center flex justify-center lg:h-screen h-full w-full home">
         <div className="pl-20 taglinediv">
-          <h1 className="text-6xl tagline w-3/5 pr-20 text-[#AABAB1] font-semibold">
+          <h1 className="text-6xl tagline w-3/5 pr-20 text-[#4A6C6F] font-semibold">
          <span className="kaarwaanbg"> Kaarwaa.N...</span><br />
 A step for the welfare of another India.
           </h1>
@@ -33,7 +33,7 @@ A step for the welfare of another India.
           </p>
           <div className="h-auto flex justify-center gap-6 py-6 w-[20rem] ">
             <Link to="/Donation">
-            <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#AABAB1] px-4 py-1 rounded-full bg-[#40b186] hover:bg-[#00684C] hover:text-[#B7D9BF] duration-300 hover:border-2 hover:border-[#24387a]">
+            <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#4A6C6F] px-4 py-1 rounded-full bg-[#40b186] hover:bg-[#00684C] hover:text-[#B7D9BF] duration-300 hover:border-2 hover:border-[#24387a]">
               Donate Now <BsArrowUpRight />
             </button>
             </Link>
@@ -51,7 +51,7 @@ A step for the welfare of another India.
       {/* about us page */}
       <div id="about" className="flex aboutdiv my-20 pt-20 flex-row">
         <div className="flex detailsdiv flex-col w-1/2">
-          <div className="w-full py-4 p-12 pl-32 h-full flex items-start abouthread text-[#AABAB1] flex-col bg-[#40B186]">
+          <div className="w-full py-4 p-12 pl-32 h-full flex items-start abouthread text-[#4A6C6F] flex-col bg-[#40B186]">
             <h1 className="py-3">About Us</h1>
             <h1 className="text-4xl font-semibold">
              A glimpse at Kaarwaa.N
@@ -80,38 +80,38 @@ A step for the welfare of another India.
           />
         </div>
       </div>
-      <div className="flex flex-col items-center h-full text-[#AABAB1] w-full px-32 py-12 bg-[#40B186] programbg">
-        <h1 className="text-4xl text-center">Program To Empower Others</h1>
+      <div className="flex flex-col items-center h-full text-[#4A6C6F] w-full px-32 py-12 bg-[#40B186] programbg">
+        <h1 className="text-6xl text-center">Counter of the Karwaa.N... works</h1>
         <p className="programdata text-center w-2/3 text-sm mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, ipsa!
-          Possimus, tempora! Expedita dicta quasi quidem accusantium temporibus
-          repudiandae voluptate odio, distinctio nesciunt, quam eum, provident
-          mollitia corrupti quisquam praesentium!
+          Karwaa.N... has taught the children along with many pro-society works
+          Like Cloth Donations, Tree Plantation, Book Donations etc.
+          Below is a counter for all the works.
+          
         </p>
         <div className="w-2/3 programdata my-10 gap-4 flex-row flex justify-center ">
           <div className="w-full h-24  flex flex-col justify-center items-center sideborder">
             <h1 className="text-4xl">24</h1>
-            <p className="text-sm">Aryan Tyagi</p>
+            <p className="text-sm">Books Donated</p>
           </div>
 
           <div className="w-full h-24  flex flex-col justify-center items-center sideborder">
             <h1 className="text-4xl">24</h1>
-            <p className="text-sm">Aryan Tyagi</p>
+            <p className="text-sm">Cloths Donated</p>
           </div>
 
           <div className="w-full h-24  flex flex-col justify-center items-center sideborder">
             <h1 className="text-4xl">24</h1>
-            <p className="text-sm">Aryan Tyagi</p>
+            <p className="text-sm">Children Selected for JNV</p>
           </div>
 
           <div className="w-full h-24  flex flex-col justify-center items-center">
             <h1 className="text-4xl">24</h1>
-            <p className="text-sm">Aryan Tyagi</p>
+            <p className="text-sm">Trees Planted</p>
           </div>
         </div>
       </div>
 
-      <h1 id="ourwork" className="text-4xl pt-20 font-semibold my-12 mt-32 text-center text-[#AABAB1]">
+      <h1 id="ourwork" className="text-4xl pt-35 font-semibold my-12 mt-32 text-center text-[#4A6C6F]">
         A Glimpse of various works by <br />
         Karwaa.N...
       </h1>
