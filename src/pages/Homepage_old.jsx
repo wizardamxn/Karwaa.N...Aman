933a8ba0868a72a09e5f2fa3c1e796671331729e
wizardamxn <148276445+wizardamxn@users.noreply.github.com>
2023-11-15 14:00:12 +0530

    EXP WITH CHATGPT

--- src/pages/Homepage_old.jsx
+++ src/pages/Homepage_old.jsx
@@ -21,9 +21,9 @@ function Homepage() {
       {/* home page  */}
       <div id="home" className="px-12 pt-20 gap-44 items-center flex justify-center lg:h-screen h-full w-full home">
         <div className="pl-20 taglinediv">
-          <h1 className="text-6xl tagline w-3/5 pr-20 text-[#4B8E95] font-semibold">
-            <span className="kaarwaanbg"> Kaarwaa.N...</span><br />
-            A step for the welfare of another India.
+          <h1 className="text-6xl tagline w-3/5 pr-20 text-[#AABAB1] font-semibold">
+         <span className="kaarwaanbg"> Kaarwaa.N...</span><br />
+A step for the welfare of another India.
           </h1>
           <p className="w-1/2 homepara mt-8 my-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
@@ -33,14 +33,14 @@ function Homepage() {
           </p>
           <div className="h-auto flex justify-center gap-6 py-6 w-[20rem] ">
             <Link to="/Donation">
-              <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#4B8E95] px-4 py-1 rounded-full bg-[#40b186] hover:bg-[#00684C] hover:text-[#B7D9BF] duration-300 hover:border-2 hover:border-[#24387a]">
-                Donate Now <BsArrowUpRight />
-              </button>
+            <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#AABAB1] px-4 py-1 rounded-full bg-[#40b186] hover:bg-[#00684C] hover:text-[#B7D9BF] duration-300 hover:border-2 hover:border-[#24387a]">
+              Donate Now <BsArrowUpRight />
+            </button>
             </Link>
             <Link to="/tshirt">
-              <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#BAD5C1] px-4 py-1 rounded-full border-[#85B491] hover:bg-[#00684C] hover:text-[#B7D9BF] duration-300">
-                Get Kaarwaa.N Tshirt
-              </button>
+            <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#BAD5C1] px-4 py-1 rounded-full border-[#85B491] hover:bg-[#00684C] hover:text-[#B7D9BF] duration-300">
+              Get Kaarwaa.N Tshirt
+            </button>
             </Link>
           </div>
         </div>
@@ -51,24 +51,24 @@ function Homepage() {
       {/* about us page */}
       <div id="about" className="flex aboutdiv my-20 pt-20 flex-row">
         <div className="flex detailsdiv flex-col w-1/2">
-          <div className="w-full py-4 p-12 pl-32 h-full flex items-start abouthread text-[#4B8E95] flex-col bg-[#40B186]">
+          <div className="w-full py-4 p-12 pl-32 h-full flex items-start abouthread text-[#AABAB1] flex-col bg-[#40B186]">
             <h1 className="py-3">About Us</h1>
             <h1 className="text-4xl font-semibold">
-              A glimpse at Kaarwaa.N
+             A glimpse at Kaarwaa.N
             </h1>
           </div>
           <div className="w-full aboutdetails py-4 p-12 pl-32 h-full">
             <p>
-              Kaarwaa.N is a social welfare association founded by the students of Jabalpur Engineering College (JEC), Jabalpur. We are dedicated to helping the poor, underprivileged, downtrodden sections of society. The primary aim of Kaarwaa.N is to tutor students for the JNV (Jawahar Navodaya Vidhyalaya) Entrance Exam and encourage “girls’ education”.
+            Kaarwaa.N is a social welfare association founded by the students of Jabalpur Engineering College (JEC), Jabalpur. We are dedicated to help poor, underprivileged, downtrodden sections of the society. The primary aim of Kaarwaa.N is to tutor students for the JNV (Jawahar Navodaya Vidhyalaya) Entrance Exam and encourage “girls’ education”.
             </p>
             <p>
-              We organize innumerable activities to help the marginalized and create awareness about various social issues. Addressing issues from health and hygiene to education and the environment, the members of Kaarwaa.N believe in working towards making this world a better place.
-              <Link to="/aboutus">
-                <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#BAD5C1] px-4 my-10 py-1 rounded-full bg-[#85B491]">
-                  Meet Our Team <BsArrowUpRight />
-                </button>
-              </Link>
+            We organize innumerous activities to help the marginalized and create awareness about various social issues. Addressing issues from health and hygiene to education and environment, the members of Kaarwaa.N believe in working towards making this world a better place.
             </p>
+          <Link to="/aboutus">
+            <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#BAD5C1] px-4 my-10 py-1 rounded-full bg-[#85B491]">
+              Meet Our Team <BsArrowUpRight />
+            </button>
+          </Link>
           </div>
         </div>
         <div className="aboutimage h-96 w-1/2">
@@ -80,7 +80,7 @@ function Homepage() {
           />
         </div>
       </div>
-      <div className="flex flex-col items-center h-full text-[#4B8E95] w-full px-32 py-12 bg-[#40B186] programbg">
+      <div className="flex flex-col items-center h-full text-[#AABAB1] w-full px-32 py-12 bg-[#40B186] programbg">
         <h1 className="text-4xl text-center">Program To Empower Others</h1>
         <p className="programdata text-center w-2/3 text-sm mt-6">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, ipsa!
@@ -111,7 +111,7 @@ function Homepage() {
         </div>
       </div>
 
-      <h1 id="ourwork" className="text-4xl pt-20 font-semibold my-12 mt-32 text-center text-[#4B8E95]">
+      <h1 id="ourwork" className="text-4xl pt-20 font-semibold my-12 mt-32 text-center text-[#AABAB1]">
         A Glimpse of various works by <br />
         Karwaa.N...
       </h1>
@@ -124,10 +124,10 @@ function Homepage() {
           />
           <div className="detailstoshow bg-[#9EA3B0]">
             <h1 className="text-xl p-4 font-semibold text-[#0D1F2D]">
-              Nukkad Natak:
+            Nukkad Natak:
             </h1>
             <p className="px-4 pb-2">
-              Nukkad nataks - a form of street theater performed by the members to create awareness amongst people regarding various social issues. 
+            Nukkad nataks - a form of street theater performed by the members to create an awareness amongst people regarding various social issues. 
             </p>
             <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#24387a] px-4 py-1 m-4 rounded-lg border-[#24387a] hover:bg-[#24387a] hover:text-white duration-300">
               Read More
@@ -142,10 +142,10 @@ function Homepage() {
           />
           <div className="detailstoshow bg-[#9EA3B0]">
             <h1 className="text-xl p-4 font-semibold text-[#0D1F2D]">
-              Blood Donation:  
+            Blood Donation:  
             </h1>
             <p className="px-4 pb-2">
-              “A single drop of blood can make a huge difference.”  Every day thousands of lives are saved when someone donates blood. 
+            “A single drop of blood can make a huge difference.”  Every day thousands of lives are saved when someone donates blood. 
             </p>
             <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#24387a] px-4 py-1 m-4 rounded-lg border-[#24387a] hover:bg-[#24387a] hover:text-white duration-300">
               Read More
@@ -160,10 +160,10 @@ function Homepage() {
           />
           <div className="detailstoshow bg-[#9EA3B0]">
             <h1 className="text-xl p-4 font-semibold text-[#0D1F2D]">
-              Plantation: 
+            Plantation: 
             </h1>
             <p className="px-4 pb-2">
-              In order to curb the consequences of deforestation, Kaarwaa.N conducts plantation drives wherein saplings are planted in different institutions.
+             In order to curb the consequences of deforestation, Kaarwaa.N conducts plantation drives wherein saplings are planted in different institutions.
             </p>
             <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#24387a] px-4 py-1 m-4 rounded-lg border-[#24387a] hover:bg-[#24387a] hover:text-white duration-300">
               Read More
@@ -178,10 +178,10 @@ function Homepage() {
           />
           <div className="detailstoshow bg-[#9EA3B0]">
             <h1 className="text-xl p-4 font-semibold text-[#0D1F2D]">
-              Clothes distribution: 
+            Clothes distribution: 
             </h1>
             <p className="px-4 pb-2">
-              Kaarwaa.N frequently donates clothes to the less fortunate ones. The clothes are received from households and then distributed to the needy by volunteers.
+           Kaarwaa.N frequently donates clothes to the less fortunate ones. The clothes are received from households and then distributed to the needy by volunteers.
             </p>
             <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#24387a] px-4 py-1 m-4 rounded-lg border-[#24387a] hover:bg-[#24387a] hover:text-white duration-300">
               Read More
@@ -196,10 +196,10 @@ function Homepage() {
           />
           <div className="detailstoshow bg-[#9EA3B0]">
             <h1 className="text-xl p-4 font-semibold text-[#0D1F2D]">
-              Orphanage visits: 
+            Orphanage visits: 
             </h1>
             <p className="px-4 pb-2">
-              The members of Kaarwaa.N occasionally visit the orphanage to spend time with the misfortunate ones/those robbed of parental love and guidance.
+           The members of Kaarwaa.N occasionally visit the orphanage to spend time with the misfortunate ones/those robbed of parental love and guidance.
             </p>
             <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#24387a] px-4 py-1 m-4 rounded-lg border-[#24387a] hover:bg-[#24387a] hover:text-white duration-300">
               Read More
@@ -214,10 +214,10 @@ function Homepage() {
           />
           <div className="detailstoshow bg-[#9EA3B0]">
             <h1 className="text-xl p-4 font-semibold text-[#0D1F2D]">
-              Exam Preparation:
+            Exam Prepration:
             </h1>
             <p className="px-4 pb-2">
-              Kaarwaa.N aims to shine light in the dark corners of the underprivileged by educating them for these life-changing exams. 
+            Kaarwaa.N aims to shine light in the dark corners of the underprivileged by educating them for these life changing exams. 
             </p>
             <button className="border-2 flex flex-row justify-center items-center gap-2 font-semibold text-[#24387a] px-4 py-1 m-4 rounded-lg border-[#24387a] hover:bg-[#24387a] hover:text-white duration-300">
               Read More
@@ -229,9 +229,9 @@ function Homepage() {
       {/* <div>
         <Tshirt />
       </div> */}
-      
+
       <div>
-        <Testimonials/>
+       <Testimonials/>
       </div>
       <Footer/>
     </>
